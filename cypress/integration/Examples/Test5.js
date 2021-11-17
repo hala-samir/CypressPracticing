1978734e115c95766633298faee7bdf5e869ae70
Hala Samir <h.samir2011@gmail.com>
2021-11-17 18:45:43 +0200

    access next sibling using next funtion

---
+++ cypress/integration/Examples/Test5.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+describe('test suite',function(){
+    it('checkout items', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('.left-align tr td:nth-child(2)').each(($el,index, $list)=>{
+            const text=$el.text()
+            if(text.includes("Python")){
+                //use next to allocat the next sibling in dom
+                cy.get('.left-align tr td:nth-child(2)').eq(index).next().then(function(price){
+                    const priceText=price.text()
+                    expect(priceText).to.equal('25')
+
+                })
+            }
+        })
+    })
+})
